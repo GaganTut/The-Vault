5812f7a699bb0e04c636fa2870cfe9734effb547
GaganTut <gstut@live.com>
2017-03-14 19:03:13 -1000

    Passed all four tests

--- vault.js
+++ vault.js
@@ -1,4 +1,22 @@
 'use strict';
 module.exports = function() {
-  
+  var secretVault = {};
+
+  function setValue(key, value) {
+    secretVault[key] = value;
+  }
+  function getValue(key) {
+    if (secretVault[key] === undefined) {
+      return null;
+    } else {
+      return secretVault[key];
+    }
+  }
+  var vaultOperations = {
+    setValue,
+    getValue
+  };
+
+
+  return vaultOperations;
 };
